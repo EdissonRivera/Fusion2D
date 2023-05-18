de0fb70ddddde3319390e179877317732c03beb8
USUARIO <edisonriveraing@gmail.com>
2023-05-18 02:58:47 -0500

    Conexion Fusion

--- FusionTest 2D/Assets/Scripts/Lobby/MiddleSectionPanel.cs
+++ FusionTest 2D/Assets/Scripts/Lobby/MiddleSectionPanel.cs
@@ -1,19 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using Fusion;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiddleSectionPanel : LobbyPanelBase
 {
+    [Header("MiddleSectionPanel vars")]
+    [SerializeField] private Button joinRandomRoomBtn;
+    [SerializeField] private Button joinRoomByArgBtn;
+    [SerializeField] private Button createRoomBtn;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField joinRoomByArgInputField;
+    [SerializeField] private TMP_InputField createRoomByArgInputField;
+
+    private NetworkRunnerController networkRunnerController;
+
+    public override void InitPanel(LobbyUIManager uiManager)
     {
-        
+        base.InitPanel(uiManager);
+
+        networkRunnerController = GlobalManagers.Instance.networkRunnerController;
+        joinRandomRoomBtn.onClick.AddListener(JoinRandomRoom);
+        joinRoomByArgBtn.onClick.AddListener(() => CreateRoom(GameMode.Client, joinRoomByArgInputField.text));
+        createRoomBtn.onClick.AddListener(()=>CreateRoom(GameMode.Host, createRoomByArgInputField.text));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateRoom(GameMode mode,string field)
     {
-        
+        if(field.Length > 2)
+        {
+            //Crear sala
+            Debug.Log($"---------{mode}---------");
+            networkRunnerController.StartGame(mode, field);
+        }
     }
+
+    private void JoinRandomRoom()
+    {
+        Debug.Log($"---------JoinRandomRoom!---------");
+        networkRunnerController.StartGame(GameMode.AutoHostOrClient,string.Empty);
+    }
+
+
 }
---
+++ FusionTest 2D/Assets/Scripts/Utils/GlobalManagers.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+public class GlobalManagers : MonoBehaviour
+{
+
+    public static GlobalManagers Instance { get; private set; }
+    
+    
+    [SerializeField] private GameObject parentObj; //se agrega manualmente en la UI]
+    [field: SerializeField] public NetworkRunnerController networkRunnerController { get; private set; }//se agrega manualmente en la UI]
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }else
+        {
+            //destroy
+            Destroy(parentObj);
+        }
+    }
+}
