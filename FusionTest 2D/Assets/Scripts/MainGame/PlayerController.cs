c6afa247d8ff9e747600f01bab3cff65e5352f4e
USUARIO <edisonriveraing@gmail.com>
2023-05-18 03:14:02 -0500

    Salto Jugadores

--- FusionTest 2D/Assets/Scripts/MainGame/PlayerController.cs
+++ FusionTest 2D/Assets/Scripts/MainGame/PlayerController.cs
@@ -3,11 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : NetworkBehaviour,IBeforeUpdate
+public class PlayerController : NetworkBehaviour, IBeforeUpdate
 {
     [SerializeField] private float moveSpeed = 6;
+    [SerializeField] private float jumpForce = 1000;
+    [Networked] private NetworkButtons buttonsPrev { get; set; }
     private float horizontal;
     private Rigidbody2D rigid;
+
+    private enum PlayerInputButtons
+    {
+        None,
+        Jump
+    }
 
     public override void Spawned()
     {
@@ -27,10 +35,23 @@ public class PlayerController : NetworkBehaviour,IBeforeUpdate
 
     public override void FixedUpdateNetwork() //Actualizacion en tiempo de red
     {
-        if (Runner.TryGetInputForPlayer<PlayerData>(Object.InputAuthority,out var input)) //extrae datos de la estructura PlayerData y valida si yo soy la autoridad de entrada de este personaje
+        if (Runner.TryGetInputForPlayer<PlayerData>(Object.InputAuthority, out var input)) //extrae datos de la estructura PlayerData y valida si yo soy la autoridad de entrada de este personaje
         {
             rigid.velocity = new Vector2(input.HorizontalInput * moveSpeed, rigid.velocity.y); //si esto s ecumple muevo al personaje
+            CheckJumpInput(input);
         }
+    }
+
+
+    private void CheckJumpInput(PlayerData input)
+    {
+        var preseed = input.NetworkButtons.GetPressed(buttonsPrev);
+        if (preseed.WasPressed(buttonsPrev, PlayerInputButtons.Jump))
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        }
+
+        buttonsPrev = input.NetworkButtons;
     }
 
     //extrae valores para asignarlos luego en locallInputPoller
@@ -38,6 +59,7 @@ public class PlayerController : NetworkBehaviour,IBeforeUpdate
     {
         PlayerData data = new PlayerData();
         data.HorizontalInput = horizontal;
+        data.NetworkButtons.Set(PlayerInputButtons.Jump, Input.GetKey(KeyCode.Space));
         return data;
     }
 
